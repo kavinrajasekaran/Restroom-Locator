59b6f0a4490f14ce74ed15cba2872056da1e6749
kavinrajasekaran <kavin.krajasekaran@gmail.com>
2024-09-28 19:57:52 -0700

    Initial Commit

---
+++ Restroom Locator/Restroom_LocatorApp.swift
@@ -0,0 +1,32 @@
+//
+//  Restroom_LocatorApp.swift
+//  Restroom Locator
+//
+//  Created by Kavin Rajasekaran on 9/28/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Restroom_LocatorApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Restroom LocatorTests/Restroom_LocatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  Restroom_LocatorTests.swift
+//  Restroom LocatorTests
+//
+//  Created by Kavin Rajasekaran on 9/28/24.
+//
+
+import Testing
+
+struct Restroom_LocatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
